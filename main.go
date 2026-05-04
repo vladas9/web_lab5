18d18b74c59545c9af33f12dfdeeef27797f3a2a
Vladislav Amza <130138352+vladas9@users.noreply.github.com>
2026-05-04 09:04:15 +0300

    add DuckDuckGo search and fix chunked transfer encoding (#2)
    Implement -s flag: fetches html.duckduckgo.com, extracts top 10 results
    with titles and URLs. Support multi-word search terms.
    Fix chunked Transfer-Encoding so response body decodes correctly.

--- main.go
+++ main.go
@@ -20,6 +20,35 @@ type Response struct {
 	Body       string
 }
 
+type SearchResult struct {
+	Title string
+	URL   string
+}
+
+func readChunked(reader *bufio.Reader) ([]byte, error) {
+	var body []byte
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			return nil, err
+		}
+		size, err := strconv.ParseInt(strings.TrimSpace(line), 16, 64)
+		if err != nil {
+			return nil, fmt.Errorf("invalid chunk size: %s", strings.TrimSpace(line))
+		}
+		if size == 0 {
+			break
+		}
+		chunk := make([]byte, size)
+		if _, err = io.ReadFull(reader, chunk); err != nil {
+			return nil, err
+		}
+		body = append(body, chunk...)
+		reader.ReadString('\n') // consume trailing CRLF
+	}
+	return body, nil
+}
+
 func readConn(conn net.Conn) (*Response, error) {
 	reader := bufio.NewReader(conn)
 
@@ -45,7 +74,12 @@ func readConn(conn net.Conn) (*Response, error) {
 		}
 	}
 
-	body, err := io.ReadAll(reader)
+	var body []byte
+	if strings.Contains(headers["transfer-encoding"], "chunked") {
+		body, err = readChunked(reader)
+	} else {
+		body, err = io.ReadAll(reader)
+	}
 	if err != nil {
 		return nil, err
 	}
@@ -69,22 +103,70 @@ func parseHTML(body string) string {
 	return extractText(doc)
 }
 
+func hasClass(n *html.Node, cls string) bool {
+	for _, a := range n.Attr {
+		if a.Key == "class" {
+			for _, c := range strings.Fields(a.Val) {
+				if c == cls {
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
+
+func extractSearchResults(body string) []SearchResult {
+	doc, _ := html.Parse(strings.NewReader(body))
+	var results []SearchResult
+
+	var traverse func(*html.Node)
+	traverse = func(n *html.Node) {
+		if len(results) >= 10 {
+			return
+		}
+		if n.Type == html.ElementNode && n.Data == "a" && hasClass(n, "result__a") {
+			title := strings.TrimSpace(extractText(n))
+			for _, a := range n.Attr {
+				if a.Key == "href" {
+					u, err := url.Parse(a.Val)
+					resultURL := a.Val
+					if err == nil {
+						if uddg := u.Query().Get("uddg"); uddg != "" {
+							resultURL, _ = url.QueryUnescape(uddg)
+						}
+					}
+					if title != "" && resultURL != "" {
+						results = append(results, SearchResult{Title: title, URL: resultURL})
+					}
+					break
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			traverse(c)
+		}
+	}
+	traverse(doc)
+	return results
+}
+
 func fetch(rawUrl string) (*Response, error) {
-	url, err := url.Parse(rawUrl)
+	u, err := url.Parse(rawUrl)
 	if err != nil {
 		return nil, err
 	}
 
 	ports := map[string]string{"http": "80", "https": "443"}
-	port, ok := ports[url.Scheme]
+	port, ok := ports[u.Scheme]
 	if !ok {
-		return nil, fmt.Errorf("unsupported scheme: %s", url.Scheme)
+		return nil, fmt.Errorf("unsupported scheme: %s", u.Scheme)
 	}
-	addr := url.Hostname() + ":" + port
+	addr := u.Hostname() + ":" + port
 
 	var conn net.Conn
-	if url.Scheme == "https" {
-		conn, err = tls.Dial("tcp", addr, &tls.Config{ServerName: url.Hostname()})
+	if u.Scheme == "https" {
+		conn, err = tls.Dial("tcp", addr, &tls.Config{ServerName: u.Hostname()})
 	} else {
 		conn, err = net.Dial("tcp", addr)
 	}
@@ -93,16 +175,29 @@ func fetch(rawUrl string) (*Response, error) {
 	}
 	defer conn.Close()
 
-	fmt.Fprintf(conn, "GET %s HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n",
-		url.RequestURI(), url.Hostname())
+	fmt.Fprintf(conn, "GET %s HTTP/1.1\r\nHost: %s\r\nUser-Agent: go2web/1.0\r\nConnection: close\r\n\r\n",
+		u.RequestURI(), u.Hostname())
 
-	resp, err := readConn(conn)
+	return readConn(conn)
+}
 
+func search(term string) error {
+	query := url.QueryEscape(term)
+	resp, err := fetch("https://html.duckduckgo.com/html/?q=" + query)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return resp, nil
+	results := extractSearchResults(resp.Body)
+	if len(results) == 0 {
+		fmt.Println("No results found.")
+		return nil
+	}
+
+	for i, r := range results {
+		fmt.Printf("%d. %s\n   %s\n\n", i+1, r.Title, r.URL)
+	}
+	return nil
 }
 
 func printHelp() {
@@ -141,7 +236,11 @@ func main() {
 			fmt.Fprintln(os.Stderr, "error: -s requires a search term")
 			os.Exit(1)
 		}
-		fmt.Println("TODO: search", args[1])
+		term := strings.Join(args[1:], " ")
+		if err := search(term); err != nil {
+			fmt.Fprintln(os.Stderr, "error:", err)
+			os.Exit(1)
+		}
 	default:
 		fmt.Fprintf(os.Stderr, "unknown flag: %s\n", args[0])
 		os.Exit(1)
